f8741138be81310a59999d9b5f3cac3010d055b5
闫韬蒙 <3094969938@qq.com>
2023-01-16 23:22:12 +0800

    feat:更新了打包配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { App } from 'vue'
 export default class  HandlyDirective{
     public options:any
     constructor(options:any){
         this.options=options
+    }
+    install(Vue:App):void{
+        console.log(Vue)
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/main.ts"),
-      name: "handly-vue-directives",
+      name: "handlyDirectives",
       // the proper extensions will be added
       fileName: "handly-vue-directives",
     },
@@ -15,7 +15,8 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ["vue"],
       output: {
-        file: "./lib/directive.js",
+        dir: "lib",
+        name:'handlyDirectives'
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: "Vue",
