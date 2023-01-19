4e9a570f315e40da111af8efa4a4a41e63318b90
闫韬蒙 <3094969938@qq.com>
2023-01-19 23:57:52 +0800

    feat:新增了懒加载的重构

--- src/directive/lazy-img.ts
+++ src/directive/lazy-img.ts
@@ -3,13 +3,19 @@
  */
 // 使用方法：
 //  <img v-lazy-img="'https://www.keaidian.com/uploads/allimg/190424/24110307_23.jpg'" alt=""></img>
+import type { Directive, DirectiveBinding } from "vue"
+interface ElType extends HTMLElement{
+    "$data_src":string;
+    "$io":IntersectionObserver
+    "src":string
+}
 export default {
-  beforeMount(el, binding) {
+  beforeMount(el:ElType, binding:DirectiveBinding) {
     el.$data_src = binding.value;
   },
-  mounted(el) {
-    const io = new IntersectionObserver((entries) => {
-      const realSrc = el.$data_src;
+  mounted(el:ElType) {
+    const io:IntersectionObserver = new IntersectionObserver((entries) => {
+      const realSrc:string = el.$data_src;
       // 通过isIntersecting判断是否在可视区域内
       entries[0].isIntersecting && realSrc && (el.src = realSrc);
     });
@@ -18,12 +24,12 @@ export default {
     // 监听目标对象
     io.observe(el);
   },
-  updated(el, binding) {
+  updated(el:ElType, binding:DirectiveBinding) {
     // 实时更新最新的图片路径
     el.$data_src = binding.value;
   },
-  unmounted(el) {
+  unmounted(el:ElType) {
     // 停止监听工作
     el.$io.disconnect();
   },
-};
+} as Directive
